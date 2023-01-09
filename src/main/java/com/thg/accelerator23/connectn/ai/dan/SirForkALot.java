5c54265420d249e32086c8cf87c6f243fd30556f
Neeraj <Neeraj.Lalji07@thehutgroup.com>
2023-01-09 15:56:53 +0000

    Changed team and AI names

--- src/main/java/com/thg/accelerator23/connectn/ai/dan/SirForkALot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/dan/SirForkALot.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.dan;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class SirForkALot extends Player {
+  public SirForkALot(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, SirForkALot.class.getName());
   }
 
   @Override
