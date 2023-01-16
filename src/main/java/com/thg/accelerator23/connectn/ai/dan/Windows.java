a4a3daf59b2320567dfd9425d4fcbe8fe53b139e
Declan Ying Hui Lee <Declan.Lee53@thehutgroup.com>
2023-01-16 11:42:41 +0000

    final changes

--- src/main/java/com/thg/accelerator23/connectn/ai/dan/Windows.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/dan/Windows.java
@@ -12,16 +12,15 @@ import java.util.Collections;
 
 public class Windows {
 
-    private int evaluateWindow(List<Position> window, Counter piece , Board board){
+    private int evaluateWindow(List<Counter> window1, Counter piece ){
         int score = 0;
-        List<Counter> window1 = listOfPositionsToCounters(window,board);
+        //List<Counter> window1 = listOfPositionsToCounters(window,board);
         int occurrencesPiece = Collections.frequency( window1, piece);
         int occurrencesNull = Collections.frequency( window1, null);
         int occurrencesOppPiece = Collections.frequency( window1, piece.getOther());
 
         if (occurrencesPiece == 4 ){
             score += 1000;
-
         }
         else if (occurrencesPiece == 3 && occurrencesNull== 1){
             score += 5;
@@ -30,7 +29,10 @@ public class Windows {
             score += 2;
         }
         else if (occurrencesOppPiece == 3 && occurrencesNull== 1){
-            score -= 4;
+            score -= 100;
+        }
+        else if (occurrencesOppPiece == 2 && occurrencesNull== 2) {
+            score -= 6;
         }
 
         return score;
@@ -53,28 +55,28 @@ public class Windows {
         //score horizontal positions
         for (int row=0;row<board.getConfig().getHeight();row++){
 
-            List<Position> positionsOnThisRow = new ArrayList<>();
+            List<Counter> positionsOnThisRow = new ArrayList<>();
             for (int col=0;col<board.getConfig().getWidth();col++){
                 Position pos = new Position(col , row);
-                positionsOnThisRow.add(pos);
+                positionsOnThisRow.add(board.getCounterAtPosition(pos));
             }
             for (int colWindow = 0;colWindow<board.getConfig().getWidth()-3;colWindow++){
-                List<Position> window = positionsOnThisRow.subList(colWindow,colWindow+n);
-                score += evaluateWindow(window,piece,board);
+                List<Counter> window = positionsOnThisRow.subList(colWindow,colWindow+n);
+                score += evaluateWindow(window,piece);
             }
 
         }
         //score vertical positions
         for (int col=0;col<board.getConfig().getWidth();col++){
 
-            List<Position> positionsOnThisRow = new ArrayList<>();
+            List<Counter> positionsOnThisRow = new ArrayList<>();
             for (int row=0;row<board.getConfig().getHeight();row++){
                 Position pos = new Position(col , row);
-                positionsOnThisRow.add(pos);
+                positionsOnThisRow.add(board.getCounterAtPosition(pos));
             }
             for (int rowWindow = 0;rowWindow<board.getConfig().getHeight()-3;rowWindow++){
-                List<Position> window = positionsOnThisRow.subList(rowWindow,rowWindow+n);
-                score += evaluateWindow(window,piece,board);
+                List<Counter> window = positionsOnThisRow.subList(rowWindow,rowWindow+n);
+                score += evaluateWindow(window,piece);
             }
         }
 
@@ -82,12 +84,12 @@ public class Windows {
         for (int row = 0; row<board.getConfig().getHeight()-3;row++ ){
 
             for (int col = 0; col<board.getConfig().getWidth()-3;col++ ){
-                List<Position> window =  new ArrayList<>();
+                List<Counter> window =  new ArrayList<>();
                 for(int i = 0;i < n;i++){
                     Position pos = new Position(col+i , row+i);
-                    window.add(pos);
+                    window.add(board.getCounterAtPosition(pos));
                 }
-                score += evaluateWindow(window,piece,board);
+                score += evaluateWindow(window,piece);
 
             }
         }
@@ -95,27 +97,27 @@ public class Windows {
         // score negative diagonal
         for (int row = 0; row<board.getConfig().getHeight()-3;row++ ){
             for (int col = 0; col<board.getConfig().getWidth()-3;col++ ){
-                List<Position> window =  new ArrayList<>();
+                List<Counter> window =  new ArrayList<>();
                 int h = board.getConfig().getHeight();
                 for(int i = 0;i < n;i++){
                     Position pos = new Position(col+i , h-1-row - i);
-                    window.add(pos);
+                    window.add(board.getCounterAtPosition(pos));
                 }
-                score += evaluateWindow(window,piece,board);
+                score += evaluateWindow(window,piece);
 
             }
         }
 
         //score the centre winning pieces (cols with possible wins from left and right)
         for (int col= 3;col<board.getConfig().getWidth()-3;col++){
-            List<Position> positionsOnThisCol =  new ArrayList<>();
+            List<Counter> positionsOnThisCol =  new ArrayList<>();
             for (int row=0;row<board.getConfig().getHeight();row++){
                 Position pos = new Position(col , row);
-                positionsOnThisCol.add(pos);
+                positionsOnThisCol.add(board.getCounterAtPosition(pos));
             }
-            List<Counter> positionsOnThisCol1=  listOfPositionsToCounters(positionsOnThisCol,board);
+            //List<Counter> positionsOnThisCol1=  listOfPositionsToCounters(positionsOnThisCol,board);
             //System.out.println(listOfPositionsToCounters(positionsOnThisCol,board));
-            int numberOurInCol =  Collections.frequency( positionsOnThisCol1, piece);
+            int numberOurInCol =  Collections.frequency( positionsOnThisCol, piece);
 
             score += numberOurInCol * 3;
 
